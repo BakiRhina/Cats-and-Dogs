1c3fa72f70251dd76ff21806e9c69eb8c80f53ed
8kyu <guillemsenabre@gmail.com>
2024-01-15 13:33:04 +0800

    train loader working

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 from torchvision import datasets
 
 PATH = "C:/Users/Ato/Documents/Programming/Python/catdog/src/datasets"
 
 train_path = PATH + "/train"
 train_set = datasets.ImageFolder(root=train_path)
-train_loader = torch.utils.data.DataLoader()
+train_loader = DataLoader(train_set, batch_size=4)
+
+
+
