55d5cde36f2cb9288084e2981a94bd45db2ce34f
8kyu <guillemsenabre@gmail.com>
2024-01-15 14:10:26 +0800

    transform added, tensor conversion, plot show debug

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-import torch
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-from torchvision import datasets
+from torchvision import datasets, transforms
 
 PATH = "C:/Users/Ato/Documents/Programming/Python/catdog/src/datasets"
 
+train_transform = transforms.Compose([
+  transforms.Resize((256,256)),
+  transforms.ToTensor(),
+])
+
 train_path = PATH + "/train"
-train_set = datasets.ImageFolder(root=train_path)
-train_loader = DataLoader(train_set, batch_size=4)
+train_set = datasets.ImageFolder(root=train_path, transform=train_transform)
+train_loader = DataLoader(train_set, batch_size=4, shuffle=True)
 
-
-
+images, labels = next(iter(train_loader))
+plt.imshow(images[0].permute(1,2,0).numpy())
+plt.show()
